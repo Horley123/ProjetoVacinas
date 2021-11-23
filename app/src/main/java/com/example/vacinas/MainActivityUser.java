79a5f14a473b38d2881192941d952b40889217a1
kassia <kassiasouzac@gmail.com>
2021-11-23 12:10:52 -0300

    Telas

--- app/src/main/java/com/example/vacinas/MainActivityUser.java
+++ app/src/main/java/com/example/vacinas/MainActivityUser.java
@@ -1,14 +1,16 @@
 package com.example.vacinas;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivityUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_user);
     }
+
 }
